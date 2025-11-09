313f58f26d770282138e52be3f2deb723ad33366
GrandGelo <skibyakoleg@gmail.com>
2025-11-09 13:34:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 
 class UkrPatentScraper:
     def __init__(self):
@@ -8,10 +6,8 @@ class UkrPatentScraper:
         chrome_options.add_argument('--headless')
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
-        chrome_options.add_argument('--disable-blink-features=AutomationControlled')
-        chrome_options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36')
+        chrome_options.binary_location = '/usr/bin/chromium-browser'
         
-        # Автоматичне встановлення ChromeDriver
-        service = Service(ChromeDriverManager().install())
+        service = Service('/usr/bin/chromedriver')
         self.driver = webdriver.Chrome(service=service, options=chrome_options)
         self.wait = WebDriverWait(self.driver, 10)
